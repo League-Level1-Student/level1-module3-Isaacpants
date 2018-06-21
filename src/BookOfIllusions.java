555bd2c31909fcf7693a872f8b9cb29c1c60608b
League Student <league@iMac-6.attlocal.net>
2018-06-21 11:58:51 -0700

    book of illusions fin

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -10,7 +10,9 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
  
 /** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
 
@@ -27,27 +29,43 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+JFrame jf = new JFrame();
+JLabel jl;
 	private void createBook() {
 		// 2. make the frame visible
+		jf.setVisible(true);
 		// 3. set the size of the frame
+		jf.setSize(500, 500);
 		// 4. find 2 images and save them to your project’s default package
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String pic1 = "images-1.jpg";
+		String pic2 = "images.jpg";
+		
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+		jl=loadImageFromComputer("images-1.jpg");
 		// 8. add your JLabel to the frame
+		jf.add(jl);
 		// 9. call the pack() method on the frame
+		jf.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		jf.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("clicked!");
 		// 12. remove everything from the frame that was added earlier
+		jf.removeAll();
 		// 13. load a new image like before (this is more than one line of code)
+		String pic2 = "images.jpg";
+		jl = loadImageFromComputer(pic2);
 		// 14. pack the frame
+		jf.pack();
 	}
 
-	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
+
 
 	/*
 	 * To use this method, the image must be placed in your Eclipse project under "default package".
@@ -61,3 +79,54 @@ public class BookOfIllusions extends MouseAdapter {
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
