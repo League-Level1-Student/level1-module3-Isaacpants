d7e7d774a1c7924df75b8642b3d8bd964516d699
Isaacpants <league@WTS14.attlocal.net>
2018-06-22 11:36:28 -0700

    cow timer fin

--- src/CowTimer.java
+++ src/CowTimer.java
@@ -1,23 +1,24 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
  */
 
-
-
 import java.applet.AudioClip;
-import java.io.IOException;
+
 import javax.swing.JApplet;
 
 public class CowTimer {
 	/*
-	 * This is an advanced recipe. There may be more than one line of code for
-	 * each instruction.
-	 * Work in seconds when testing, then change to minutes
+	 * This is an advanced recipe. There may be more than one line of code for each
+	 * instruction. Work in seconds when testing, then change to minutes
 	 */
 
 	public static void main(String[] args) throws InterruptedException {
 		/* 1. Make a CowTimer, set the time and start it. */
+		CowTimer ct = new CowTimer();
+		ct.setTime(20);
+		ct.start();
 
 	}
 
@@ -30,25 +31,26 @@ public class CowTimer {
 
 	public void start() throws InterruptedException {
 		/*
-		 * 2. Count down the minutes, print the current minute then sleep for 60
-		 * seconds using Thread.sleep(int milliseconds).
+		 * 2. Count down the minutes, print the current minute then sleep for 60 seconds
+		 * using Thread.sleep(int milliseconds).
 		 */
+		for (int i = 0; i < minutes; i++) {
+			Thread.sleep(600000);
 
+			System.out.println(minutes - i);
+		}
 		/*
-		 * 3. When the timer is finished, use the playSound method to play a moo
-		 * sound. You can use the .wav file in the default package, or you can download 
-		 * one from freesound.org, then drag it intothe default package. 
+		 *
 		 */
-
+		playSound("moo.wav");
 	}
 
 	private void playSound(String fileName) {
-		AudioClip sound = JApplet
-				.newAudioClip(getClass().getResource(fileName));
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
 		sound.play();
 	}
 
-private void speak(String stuffToSay) {
+	private void speak(String stuffToSay) {
 		try {
 			Runtime.getRuntime().exec("say " + stuffToSay).waitFor();
 		} catch (Exception e) {
@@ -56,6 +58,8 @@ private void speak(String stuffToSay) {
 		}
 	}
 
-
 }
 
+// 3. When the timer is finished, use the playSound method to play a moo sound.
+// * You can use the .wav file in the default package, or you can download one
+// * from freesound.org, then drag it intothe default package.
